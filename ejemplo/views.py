2fcccd850a1fb4bf4feefcfa0499d8b23941353d
Martín Maddalena <martinmadda@gmail.com>
2022-10-17 20:57:22 -0300

    segundo commit de mi proyecto

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
-from django.shortcuts import render
-
 def index(request):
     return render(request, "ejemplo/saludar.html")
+
+def index_dos(request, nombre, apellido):
+    return render(request, "ejemplo/saludar.html",
+    {
+        "nombre": nombre,
+        "apellido": apellido,
+    })
