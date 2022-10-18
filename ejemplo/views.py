0e411f58b52f6309a8574103ede67711a3b69cd1
Martín Maddalena <martinmadda@gmail.com>
2022-10-17 22:01:54 -0300

    tercer commit de mi proyecto

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -9,3 +9,13 @@ def index_dos(request, nombre, apellido):
         "nombre": nombre,
         "apellido": apellido,
     })
+
+def index_tres(request):
+    return render (request, "ejemplo/saludar.html",
+    {"notas":[1,2,3,4,5,6,7,8]}
+    )
+
+def imc(request):
+    imc = 1
+    return render(request, "ejemplo/imc.html",{"imc": imc})
+    
